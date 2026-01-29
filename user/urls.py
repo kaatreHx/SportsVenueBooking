c3dfee1063465b70dcd53f0ec3496a94e035689e
KaatreHx <helloworld00ff@gmail.com>
2026-01-29 17:53:33 +0545

    feat: added all apis url

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import apis
+from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', apis.UserAPIView, basename='user')
 
 urlpatterns = [
-    path('send-sms/', apis.send_test_sms, name='send-sms'),
+    path('register/', apis.RegisterAPIView.as_view(), name='register'),
+    path('verify-otp/', apis.VerifyOTP.as_view(), name='verify_otp'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh-token/', jwt_views.TokenRefreshView.as_view(), name='refresh'),
+    path('accounts/', include(router.urls))
 ]
