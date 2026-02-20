5e29484f2894344b12c17c06ce7e47999e5e93bb
KaatreHx <helloworld00ff@gmail.com>
2026-02-20 17:50:29 +0545

    updated url file

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('', include('attachment.urls')),
+    # path('', include('attachment.urls')),
+    path('futsal/', include('futsal.urls')),
     # Swagger UI URLs:
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
