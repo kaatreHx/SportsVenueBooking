bf62045aedddefe1a9029fbeddfeb37292819ece
KaatreHx <helloworld00ff@gmail.com>
2026-03-02 14:45:11 +0545

    updated futsal serializer

--- futsal/serializers.py
+++ futsal/serializers.py
@@ -3,24 +3,38 @@ from .models import Futsal
 from user.serializers import UserSerializer
 
 class FutsalSerializer(serializers.ModelSerializer):
-    image_id = serializers.ListField(child=serializers.UUIDField(), required=False)
     user = UserSerializer(read_only=True)
+
     class Meta:
         model = Futsal
-        fields = '__all__'
-        read_only_fields = ('uuid', 'user', 'created_at')
-    
+        fields = "__all__"
+        read_only_fields = ("uuid", "user", "created_at")
+
+    def validate(self, attrs):
+        request = self.context.get("request")
+        user = getattr(request, "user", None)
+
+        if request and request.method == "POST":
+            if not user or not user.is_vendor:
+                raise serializers.ValidationError(
+                    "You are not authorized to create a futsal."
+                )
+
+            if Futsal.objects.filter(user=user).exists():
+                raise serializers.ValidationError(
+                    "You have already created a futsal."
+                )
+
+        return attrs
+
     def create(self, validated_data):
-        # image_ids = validated_data.pop('image_id', [])
-        user = self.context['request'].user
-        
-        # Assign user before creation to avoid IntegrityError
-        futsal = Futsal.objects.create(user=user, **validated_data)
-        
-        # for image_id in image_ids:
-        #     content_type = ContentType.objects.get_for_model(Futsal)
-        #     Attachment.objects.create(content_type=content_type, object_id=futsal.uuid, file=image_id)
-        return futsal
-    
-    # def update(self, instance, validated_data):
+        request = self.context.get("request")
+        user = request.user
+        return Futsal.objects.create(user=user, **validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
         
