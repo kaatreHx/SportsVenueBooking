d5fdccb8f4f0d1fe71afa4054c9a1d87bb07d3ad
KaatreHx <helloworld00ff@gmail.com>
2026-03-05 16:26:49 +0545

    updated futsal venue fields

--- futsal/models.py
+++ futsal/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericRelation
 import uuid
+from .enum import ImageType, VenueApprovalStatus
+
 # from attachment.models import Attachment
 
 class Futsal(models.Model):
@@ -24,10 +26,24 @@ class Futsal(models.Model):
     contact_number_secondary = models.CharField(max_length=15, blank=True, null=True)
     price_per_hour = models.DecimalField(max_digits=8, decimal_places=2)
     is_active = models.BooleanField(default=True)
+    approval_status = models.CharField(
+        max_length=50,
+        choices=[(tag.value, tag.value) for tag in VenueApprovalStatus],
+        default=VenueApprovalStatus.PENDING.value
+    )
+    # amenities
+    wifi = models.BooleanField(default=False)
+    washroom = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+
+    # timing
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"Name {self.name} - Time {self.opening_time} - {self.closing_time}"
 
 def futsal_image_path(instance, filename):
     return f"futsal_images/{instance.futsal.user.uuid}/{filename}"
@@ -44,4 +60,9 @@ class FutsalImage(models.Model):
         on_delete=models.CASCADE
     )
     image = models.ImageField(upload_to=futsal_image_path)
+    image_type = models.CharField(
+        max_length=50,
+        choices=[(tag.value, tag.value) for tag in ImageType],
+        default=ImageType.PICTURE.value
+    )
     created_at = models.DateTimeField(auto_now_add=True)
