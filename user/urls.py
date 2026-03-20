aad9889d67c9fca150c4dcf2e6ce2364a06307c8
kaatreHx <helloworld00ff@gmail.com>
2026-03-20 16:49:24 +0545

    updated urls

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import apis
 from rest_framework_simplejwt import views as jwt_views
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -10,7 +9,7 @@ router.register(r'users', apis.UserAPIView, basename='user')
 auth_patterns = [
     path('register/', apis.RegisterAPIView.as_view(), name='register'),
     path('verify-otp/', apis.VerifyOTP.as_view(), name='verify_otp'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/', apis.CustomLoginView.as_view(), name='login'),
     path('refresh-token/', jwt_views.TokenRefreshView.as_view(), name='refresh'),
 ]
 
